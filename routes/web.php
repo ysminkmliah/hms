4d8a0cd59beac5769c8ed3f43804c17e9a30eeff
Yasmin <yazmin5204@gmail.com>
2022-06-16 15:45:48 +0800

    user login register

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class,'index']);
+Route::get('/home', [HomeController::class,'home']);
 
 Route::middleware([
     'auth:sanctum',
