4cebafdcd2fcbf75e34cf011a9741148a42ba81e
Yasmin <yazmin5204@gmail.com>
2022-06-17 10:12:03 +0800

    update user

--- resources/views/user/news.blade.php
+++ resources/views/user/news.blade.php
@@ -1,61 +1,64 @@
 <div class="page-section bg-light">
-    <div class="container">
-      <h1 class="text-center wow fadeInUp">Latest News</h1>
-      <div class="row mt-5">
-        <div class="col-lg-4 py-2 wow zoomIn">
-          <div class="card-blog">
-            <div class="header">
-              <div class="post-category">
-                <a href="#">Covid19</a>
-              </div>
-              <a href="blog-details.html" class="post-thumb">
-                <img src="../assets/img/blog/blog_1.jpg" alt="">
-              </a>
+  <div class="container">
+    <h1 class="text-center wow fadeInUp">Latest News</h1>
+    <div class="row mt-5">
+
+      <div class="col-lg-4 py-2 wow zoomIn">
+        <div class="card-blog">
+          <div class="header">
+            <div class="post-category">
+              <a href="#">Covid19</a>
             </div>
-            <div class="body">
-              <h5 class="post-title"><a href="blog-details.html">List of Countries without Coronavirus case</a></h5>
-              <div class="site-info">
-                <div class="avatar mr-2">
-                  <div class="avatar-img">
-                    <img src="../assets/img/person/person_1.jpg" alt="">
-                  </div>
-                  <span>Roger Adams</span>
+            <a href="blog-details.html" class="post-thumb">
+              <img src="../assets/img/blog/blog_1.jpg" alt="">
+            </a>
+          </div>
+          <div class="body">
+            <h5 class="post-title"><a href="blog-details.html">List of Countries without Coronavirus case</a></h5>
+            <div class="site-info">
+              <div class="avatar mr-2">
+                <div class="avatar-img">
+                  <img src="../assets/img/person/person_1.jpg" alt="">
                 </div>
-                <span class="mai-time"></span> 1 week ago
+                <span>Roger Adams</span>
               </div>
+              <span class="mai-time"></span> 1 week ago
             </div>
           </div>
         </div>
-        <div class="col-lg-4 py-2 wow zoomIn">
-          <div class="card-blog">
-            <div class="header">
-              <div class="post-category">
-                <a href="#">Covid19</a>
-              </div>
-              <a href="blog-details.html" class="post-thumb">
-                <img src="../assets/img/blog/blog_2.jpg" alt="">
-              </a>
+      </div>
+
+      <div class="col-lg-4 py-2 wow zoomIn">
+        <div class="card-blog">
+          <div class="header">
+            <div class="post-category">
+              <a href="#">Covid19</a>
             </div>
-            <div class="body">
-              <h5 class="post-title"><a href="blog-details.html">Recovery Room: News beyond the pandemic</a></h5>
-              <div class="site-info">
-                <div class="avatar mr-2">
-                  <div class="avatar-img">
-                    <img src="../assets/img/person/person_1.jpg" alt="">
-                  </div>
-                  <span>Roger Adams</span>
+            <a href="blog-details.html" class="post-thumb">
+              <img src="../assets/img/blog/blog_2.jpg" alt="">
+            </a>
+          </div>
+          <div class="body">
+            <h5 class="post-title"><a href="blog-details.html">Recovery Room: News beyond the pandemic</a></h5>
+            <div class="site-info">
+              <div class="avatar mr-2">
+                <div class="avatar-img">
+                  <img src="../assets/img/person/person_1.jpg" alt="">
                 </div>
-                <span class="mai-time"></span> 4 weeks ago
+                <span>Roger Adams</span>
               </div>
+              <span class="mai-time"></span> 4 weeks ago
             </div>
           </div>
         </div>
-        <div class="col-lg-4 py-2 wow zoomIn">
-          <div class="card-blog">
-            <div class="header">
-              <div class="post-category">
-                <a href="#">Covid19</a>
-              </div>
+      </div>
+
+      <div class="col-lg-4 py-2 wow zoomIn">
+        <div class="card-blog">
+          <div class="header">
+            <div class="post-category">
+              <a href="#">Covid19</a>
+            </div>
               <a href="blog-details.html" class="post-thumb">
                 <img src="../assets/img/blog/blog_3.jpg" alt="">
               </a>
@@ -79,4 +82,5 @@
         </div>
       </div>
     </div>
-  </div> <!-- .page-section -->
+  </div>
+</div> <!-- .page-section -->
